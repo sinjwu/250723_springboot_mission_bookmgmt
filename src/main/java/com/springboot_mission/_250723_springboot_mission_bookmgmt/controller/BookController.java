5dae70cb650c33aa7442e46f0216b757080cf11a
sinjwu <harbirdouk@gmail.com>
2025-07-24 17:54:17 +0900

    250723 springboot mission book management

--- src/main/java/com/springboot_mission/_250723_springboot_mission_bookmgmt/controller/BookController.java
+++ src/main/java/com/springboot_mission/_250723_springboot_mission_bookmgmt/controller/BookController.java
@@ -15,7 +15,8 @@ public class BookController {
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
     @GetMapping
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
         return "books";
     }
     @GetMapping("/books/new")
@@ -27,6 +28,18 @@ public class BookController {
     @PostMapping("/books")
     public String save(@ModelAttribute Book book) {
         bookRepository.save(book);
+        return "redirect:/";
+    }
+    @GetMapping("/books/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        model.addAttribute("book", bookRepository.findById(id));
+        model.addAttribute("authors", authorRepository.finalAll());
+        return "book_form";
+    }
+    @PostMapping("/books/{id}/edit")
+    public String update(@PathVariable Long id, @ModelAttribute Book book) {
+        book.setId(id);
+        bookRepository.update(book);
         return "redirect:/";
     }
     @PostMapping("/books/{id}/delete")
--- src/main/java/com/springboot_mission/_250723_springboot_mission_bookmgmt/repository/BookRepository.java
+++ src/main/java/com/springboot_mission/_250723_springboot_mission_bookmgmt/repository/BookRepository.java
@@ -32,10 +32,22 @@ public class BookRepository {
                 mapper
         );
     }
+    public Book findById(Long id) {
+        return jdbcTemplate.queryForObject(
+                "SELECT b.*, a.name AS author_name FROM books b JOIN authors a ON b.author_id = a.id WHERE b.id = ?",
+                mapper, id
+        );
+    }
     public void save(Book book) {
         jdbcTemplate.update(
                 "INSERT INTO books(title, author_id, published_date) VALUES(?,?,?)",
                 book.getTitle(), book.getAuthorId(), book.getPublishedDate()
+        );
+    }
+    public void update(Book book) {
+        jdbcTemplate.update(
+                "UPDATE books SET title=?, author_id=?, published_date=? WHERE id=?",
+                book.getTitle(), book.getAuthorId(), book.getPublishedDate(), book.getId()
         );
     }
     public void delete(Long id) {
